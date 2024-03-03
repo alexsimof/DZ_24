3dd4b43cc4325033cd5a57e049948eb03a9f75e1
Alex Simonov <alex.simonof@gmail.com>
2024-03-04 00:55:10 +0200

    add second script

--- js/script.js
+++ js/script.js
@@ -1,8 +1,8 @@
 
-let rateIn = document.getElementById('input-rate')
-let sumIn = document.getElementById('input-sum')
-let result = document.getElementById('result')
-let btn = document.getElementById('btn')
+let rateIn = document.getElementById('input-rate');
+let sumIn = document.getElementById('input-sum');
+let result = document.getElementById('result');
+let btn = document.getElementById('btn');
 
 function createCurrencyConverter(rate) {
 
@@ -18,8 +18,46 @@ function createCurrencyConverter(rate) {
 }
 
 btn.onclick = function () {
-  let converter = createCurrencyConverter(rateIn.value)
-  let res = converter.toLocalCurrency(sumIn.value)
-  result.innerText = ` = ${Math.trunc(res)}`
+  let converter = createCurrencyConverter(rateIn.value);
+  let res = converter.toLocalCurrency(sumIn.value);
+  result.innerText = ` = ${Math.trunc(res)}`;
 
 }
+
+// task second
+
+function createInvestmentAccount(initialAmount, annualInterestRate) {
+  let cash = initialAmount
+  let rate = annualInterestRate
+  let myDeposit = cash
+
+
+  function deposit(amount) {
+    return amount = myDeposit + amount
+  }
+
+  function withdraw(amount) {
+    return amount = myDeposit - amount
+  }
+
+  function calculateProfit(years = 1) {
+    return profit = (cash/100*rate + cash)*years
+  }
+
+  function getAccountInfo() {
+    console.log(myDeposit)
+    console.log(rate)
+    console.log(calculateProfit())
+  }
+
+  return {
+    deposit: deposit,
+    withdraw: withdraw,
+    calculateProfit: calculateProfit,
+    getAccountInfo: getAccountInfo,
+  }
+}
+let myAccount = createInvestmentAccount(1500, 3.5);
+// console.log(myAccount.deposit(500))
+// myAccount.getAccountInfo()
+console.log(myAccount.calculateProfit(1))
